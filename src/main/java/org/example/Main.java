54d600f208ca63887739b0e4f0492384be8c4f2b
Paulohmb <smpbastos@gmail.com>
2026-06-09 21:27:40 -0300

    Cbancaria2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,11 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         ContaBancaria minhaGrana = new ContaBancaria();
         minhaGrana.depositar(500);
-        System.out.println("Tenho R$: " + minhaGrana.getSaldo() );
+        System.out.println("Tenho R$ " + minhaGrana.getSaldo() );
+        minhaGrana.sacar(100);
+        System.out.println("Sobrou R$ " + minhaGrana.getSaldo() );
     }
 }
